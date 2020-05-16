fcb934b531a22800278cb4badda79a267fb79857
xuyiming <xuyimingwork@foxmail.com>
2020-05-16 17:58:53 +0800

    chunked transfer encoding use 16 radix

--- projects/toy-browser/client.js
+++ projects/toy-browser/client.js
@@ -180,8 +180,8 @@ class TrunkedBodyParser {
           this.current = this.WAITING_LENGTH_LINE_END
         }
       } else {
-        this.length *= 10
-        this.length += char.charCodeAt(0) - '0'.charCodeAt(0)
+        this.length *= 16
+        this.length += parseInt(char, 16)
       }
     } else if (this.current === this.WAITING_LENGTH_LINE_END) {
       if (char === '\n') {
--- projects/toy-browser/server.js
+++ projects/toy-browser/server.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('X-Foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('ok');
+  res.end('1234567890asdfgh');
 })
 .listen(8088)
